4279658197ead7ef2c86da34febf76fbe1cf6e44
Julien Heroguelle <julienheroguelle@MacBook-Pro-de-Julien.local>
2018-11-12 12:08:39 +0100

    Chapter 7.1.5 (property binding on component property)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  mjVinyl = {
+    title: 'Bad',
+    artiste: 'Michael Jackson',
+    imageUrl: 'https://img-4.linternaute.com/PI-AYLwdrSAlAsBVE53jZasrWYM=/350x/smart/4184ae2fdda74cb5b2e150bc98efebe8/ccmcms-linternaute/19006.jpg',
+    releaseDate: 1987
+  };
   title = 'vinyl-bookshelf';
 }
--- src/app/vinyl/components/vinyl-preview/vinyl-preview.component.ts
+++ src/app/vinyl/components/vinyl-preview/vinyl-preview.component.ts
@@ -7,12 +7,8 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class VinylPreviewComponent implements OnInit {
 
-  public vinyl: any = {
-    title: 'Bad',
-    artiste: 'Michael Jackson',
-    imageUrl: 'https://img-4.linternaute.com/PI-AYLwdrSAlAsBVE53jZasrWYM=/350x/smart/4184ae2fdda74cb5b2e150bc98efebe8/ccmcms-linternaute/19006.jpg',
-    releaseDate: 1987
-  };
+  @Input()
+  public vinyl: any;
 
   constructor() { }
 
